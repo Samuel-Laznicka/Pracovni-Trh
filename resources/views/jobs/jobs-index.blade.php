a2c3b7271236ec505752cb705fea7549888abba1
SamuelL <161542454+Samuel-Laznicka@users.noreply.github.com>
2024-08-29 15:47:39 +0200

    Update jobs-index.blade.php

--- resources/views/jobs/jobs-index.blade.php
+++ resources/views/jobs/jobs-index.blade.php
@@ -3,14 +3,18 @@
         
         <section class="pt-10"> <!--mt-6 space-y-6-->
                 <div>
-                    @foreach ($jobs as $job)
-                        <x-job-card-wide :$job />
-                        <a href="/jobs/{{ $job->id }}" :$job>
-                            <x-forms.button class="mt-1 mb-5">
-                                Zobrazit pracovní pozici
-                            </x-forms.button>
-                        </a>
-                    @endforeach
+                    @if($jobs->isEmpty)
+                        <h3 class="font-bold text-xl text-center">Žádné dostupné pozice!</h3>
+                    @else
+                        @foreach ($jobs as $job)
+                            <x-job-card-wide :$job />
+                            <a href="/jobs/{{ $job->id }}" :$job>
+                                <x-forms.button class="mt-1 mb-5">
+                                    Zobrazit pracovní pozici
+                                </x-forms.button>
+                            </a>
+                        @endforeach
+                    @endif
                 </div>
         </section>
         
