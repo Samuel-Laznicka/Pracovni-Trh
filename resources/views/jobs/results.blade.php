1200b618d2c5a588a3fdc244b0f8ff1e235b7e9e
SamuelL <161542454+Samuel-Laznicka@users.noreply.github.com>
2024-08-29 15:59:41 +0200

    Update results.blade.php

--- resources/views/jobs/results.blade.php
+++ resources/views/jobs/results.blade.php
@@ -4,7 +4,7 @@
     <x-page-heading>Výsledky</x-page-heading>
 
     <div class="space-y-6">
-        @if($jobs->isEmpty)
+        @if($jobs->isEmpty())
             <h3 class="font-bold text-xl text-center"> Žádné odpovídající výsledky!</h3>
         @else
             @foreach($jobs as $job)
