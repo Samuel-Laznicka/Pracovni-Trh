a4bd88dc49c34c24d18d9db5596432f0526b8020
SamuelL <161542454+Samuel-Laznicka@users.noreply.github.com>
2024-08-29 16:14:35 +0200

    Update companies.blade.php

--- resources/views/jobs/companies.blade.php
+++ resources/views/jobs/companies.blade.php
@@ -3,7 +3,7 @@
         
         <section class="pt-10"> <!--mt-6 space-y-6-->
             @if($jobs->isEmpty())
-                <h3 class="font-bold text-xl text-center"></h3>
+                <h3 class="font-bold text-xl text-center">Žádné registrované firmy!</h3>
             @else
                 <div class="mt-6 space-y-6">
                     @foreach ($jobs as $job)
