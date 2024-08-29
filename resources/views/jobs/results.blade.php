caa361822d337a132deb2ae26e97e55bd031eda7
SamuelL <161542454+Samuel-Laznicka@users.noreply.github.com>
2024-08-29 15:48:07 +0200

    Update results.blade.php

--- resources/views/jobs/results.blade.php
+++ resources/views/jobs/results.blade.php
@@ -4,8 +4,12 @@
     <x-page-heading>Výsledky</x-page-heading>
 
     <div class="space-y-6">
-        @foreach($jobs as $job)
-            <x-job-card-wide :$job />
-        @endforeach
+        @if($jobs->isEmpty)
+            <h3 class="font-bold text-xl text-center"> Žádné odpovídající výsledky!</h3>
+        @else
+            @foreach($jobs as $job)
+                <x-job-card-wide :$job />
+            @endforeach
+        @endif
     </div>
 </x-layout>
