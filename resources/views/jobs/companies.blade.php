6472a6d08cbe00f34254d57f2e8de679ea427f0f
SamuelL <161542454+Samuel-Laznicka@users.noreply.github.com>
2024-08-29 15:46:47 +0200

    Update companies.blade.php

--- resources/views/jobs/companies.blade.php
+++ resources/views/jobs/companies.blade.php
@@ -2,11 +2,15 @@
         <x-page-heading>Nabízené pozice</x-page-heading>
         
         <section class="pt-10"> <!--mt-6 space-y-6-->
+            @if($jobs->isEmpty())
+                <h3 class="font-bold text-xl text-center"></h3>
+            @else
                 <div class="mt-6 space-y-6">
                     @foreach ($jobs as $job)
                         <x-company-card :$job/>
                     @endforeach
                 </div>
+            @endif
         </section>
         
         <div>
